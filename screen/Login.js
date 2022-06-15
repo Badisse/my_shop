5d4158693f59f85a516141c810bb743796c4f332
Badisse <badisse.bouabdallahb@laposte.net>
2022-06-15 23:22:00 +0200

    feat: add login informations

--- screen/Login.js
+++ screen/Login.js
@@ -1,11 +1,30 @@
-import { StyleSheet, View, Text } from "react-native"
+import React, { useState } from "react";
+import { StyleSheet, StatusBar, View, Text, TextInput } from "react-native";
 
 const Login = () => {
-    return(
-        <View style={styles.container}>
-            <Text>Login</Text>
-        </View>
-    )
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
+        <>
+            <StatusBar barStyle="default" />
+            <View style={styles.container}>
+                <Text>Login</Text>
+                <TextInput
+                    placeholder="Email"
+                    onChangeText={newEmail => setEmail(newEmail)}
+                    defaultValue={email}
+                />
+                <TextInput
+                    placeholder="Password"
+                    onChangeText={newPassword => setPassword(newPassword)}
+                    defaultValue={password}
+                    secureTextEntry={true}
+                />
+            </View>
+        </>
+
+    );
 }
 
 export default Login;
